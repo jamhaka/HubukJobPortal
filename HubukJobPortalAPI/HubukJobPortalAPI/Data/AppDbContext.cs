2da28669bf432dff5ffff403bf6b29f37339dce1
abdulquddus nuhu <abdulquddusnuhu@gmail.com>
2022-01-19 11:56:58 +0100

    Add DataFolder & EFCore

---
+++ HubukJobPortalAPI/HubukJobPortalAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HubukJobPortalAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly DbContextOptions _options;
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            _options = options;
+        }
+    }
+}
--- HubukJobPortalAPI/HubukJobPortalAPI/Program.cs
+++ HubukJobPortalAPI/HubukJobPortalAPI/Program.cs
@@ -1,6 +1,12 @@
+using HubukJobPortalAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Add DbContext service
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=Onimisi;Database=HubukJobPortalAPI"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
